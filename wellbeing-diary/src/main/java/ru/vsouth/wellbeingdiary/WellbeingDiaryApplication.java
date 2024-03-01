879d4fa611201fd8b0d526156d34f1bdfa89882e
vsouth <neppchela@gmail.com>
2024-03-01 22:51:58 +0300

    Initial commit

---
+++ wellbeing-diary/src/main/java/ru/vsouth/wellbeingdiary/WellbeingDiaryApplication.java
@@ -0,0 +1,13 @@
+package ru.vsouth.wellbeingdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellbeingDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellbeingDiaryApplication.class, args);
+	}
+
+}
---
+++ wellbeing-diary/src/test/java/ru/vsouth/wellbeingdiary/WellbeingDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsouth.wellbeingdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellbeingDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
