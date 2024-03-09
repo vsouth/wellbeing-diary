36eafb6101e918dc8b27380866b6345e0bf77a01
vsouth <neppchela@gmail.com>
2024-03-10 00:56:59 +0300

    added DiaryManagementService to manage work of all entry services

--- wellbeing-diary/src/main/java/ru/vsouth/wellbeingdiary/controller/DiaryController.java
+++ wellbeing-diary/src/main/java/ru/vsouth/wellbeingdiary/controller/DiaryController.java
@@ -4,58 +4,58 @@ import org.springframework.web.bind.annotation.*;
 import ru.vsouth.wellbeingdiary.dto.DiaryEntryResponse;
 import ru.vsouth.wellbeingdiary.dto.OpenDiaryEntryResponse;
 import ru.vsouth.wellbeingdiary.model.DiaryEntry;
-import ru.vsouth.wellbeingdiary.service.DiaryEntryServiceImpl;
+import ru.vsouth.wellbeingdiary.service.diary.DiaryManagementService;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/diary")
-public class DiaryEntryController {
-    private final DiaryEntryServiceImpl diaryEntryService;
+public class DiaryController {
+    private final DiaryManagementService diaryManagementService;
 
-    public DiaryEntryController(DiaryEntryServiceImpl diaryEntryService) {
-        this.diaryEntryService = diaryEntryService;
+    public DiaryController(DiaryManagementService diaryManagementService) {
+        this.diaryManagementService = diaryManagementService;
     }
 
     @GetMapping("/open_list")
     public List<OpenDiaryEntryResponse> getAllOpenEntries() {
-        return diaryEntryService.getAllOpenEntries();
+        return diaryManagementService.getOpenDiaryEntries();
     }
 
     @GetMapping("/list")
     public List<DiaryEntryResponse> getAllEntries() {
-        return diaryEntryService.getAllEntries();
+        return diaryManagementService.getDiaryEntries();
     }
 
     @GetMapping("/{id}")
     public DiaryEntryResponse findEntryById(@PathVariable int id) {
-        return diaryEntryService.getEntryById(id);
+        return diaryManagementService.getDiaryEntry(id);
     }
 
     @GetMapping("/{user_id}/list")
     public List<DiaryEntryResponse> findAllEntriesByUserId(@PathVariable("user_id") int userId) {
-        return diaryEntryService.getEntriesByUserId(userId);
+        return diaryManagementService.getDiaryEntries(userId);
     }
 
 
     @GetMapping("/{user_id}/{diary_entry_id}")
     public DiaryEntryResponse findEntryByUserId(@PathVariable("user_id") int userId, @PathVariable("diary_entry_id") int diaryEntryId) {
-        return diaryEntryService.getEntryByUserIdAndDiaryEntryId(userId, diaryEntryId);
+        return diaryManagementService.getDiaryEntry(userId, diaryEntryId);
     }
 
     @PutMapping("/update")
     public DiaryEntryResponse updateEntry(@RequestBody DiaryEntry diaryEntry) {
-        return diaryEntryService.updateEntry(diaryEntry);
+        return diaryManagementService.updateDiaryEntry(diaryEntry);
     }
 
 
     @DeleteMapping("/delete/{id}")
     public DiaryEntryResponse deleteEntry(@PathVariable int id) {
-        return diaryEntryService.deleteEntry(id);
+        return diaryManagementService.deleteDiaryEntry(id);
     }
 
     @PostMapping("/")
     public DiaryEntryResponse addEntry(@RequestBody DiaryEntry diaryEntry) {
-        return diaryEntryService.saveEntry(diaryEntry);
+        return diaryManagementService.addDiaryEntry(diaryEntry);
     }
 }
---
+++ wellbeing-diary/src/main/java/ru/vsouth/wellbeingdiary/service/diary/DiaryManagementService.java
@@ -0,0 +1,54 @@
+package ru.vsouth.wellbeingdiary.service.diary;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import ru.vsouth.wellbeingdiary.dto.DiaryEntryResponse;
+import ru.vsouth.wellbeingdiary.dto.OpenDiaryEntryResponse;
+import ru.vsouth.wellbeingdiary.model.DiaryEntry;
+import ru.vsouth.wellbeingdiary.service.diary.diaryentry.DiaryEntryService;
+import ru.vsouth.wellbeingdiary.service.diary.healthentry.HealthEntryService;
+
+import java.util.List;
+
+@Service
+public class DiaryManagementService {
+    private final DiaryEntryService diaryEntryService;
+    private final HealthEntryService healthEntryService;
+
+    public DiaryManagementService(DiaryEntryService diaryEntryService, HealthEntryService healthEntryService) {
+        this.diaryEntryService = diaryEntryService;
+        this.healthEntryService = healthEntryService;
+    }
+
+    public List<OpenDiaryEntryResponse> getOpenDiaryEntries() {
+        return diaryEntryService.getAllOpenEntries();
+    }
+
+    public List<DiaryEntryResponse> getDiaryEntries() {
+        return diaryEntryService.getAllEntries();
+    }
+
+    public List<DiaryEntryResponse> getDiaryEntries(int userId) {
+        return diaryEntryService.getEntriesByUserId(userId);
+    }
+
+    public DiaryEntryResponse getDiaryEntry(int userId, int diaryEntryId) {
+        return diaryEntryService.getEntryByUserIdAndDiaryEntryId(userId, diaryEntryId);
+    }
+
+    public DiaryEntryResponse getDiaryEntry(int id) {
+        return diaryEntryService.getEntryById(id);
+    }
+
+    public DiaryEntryResponse updateDiaryEntry(DiaryEntry diaryEntry) {
+        return diaryEntryService.updateEntry(diaryEntry);
+    }
+
+    public DiaryEntryResponse deleteDiaryEntry(int id) {
+        return diaryEntryService.deleteEntry(id);
+    }
+
+    public DiaryEntryResponse addDiaryEntry(DiaryEntry diaryEntry) {
+        return diaryEntryService.saveEntry(diaryEntry);
+    }
+}
