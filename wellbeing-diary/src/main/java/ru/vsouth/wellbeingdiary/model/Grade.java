99a32d38d52dbb98699169b04bbe70d7a93a64da
vsouth <neppchela@gmail.com>
2024-03-12 00:36:02 +0300

    refactored enums

--- wellbeing-diary/src/main/java/ru/vsouth/wellbeingdiary/model/Grade.java
+++ wellbeing-diary/src/main/java/ru/vsouth/wellbeingdiary/model/Grade.java
@@ -1,20 +1,10 @@
 package ru.vsouth.wellbeingdiary.model;
 
 public enum Grade {
-    AWFUL("AWFUL"),
-    BAD("BAD"),
-    NORMAL("NORMAL"),
-    GOOD("GOOD"),
-    EXCELLENT("EXCELLENT"),
+    AWFUL,
+    BAD,
+    NORMAL,
+    GOOD,
+    EXCELLENT,
     ;
-
-    private String name;
-
-    Grade(String name) {
-        this.name = name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- wellbeing-diary/src/main/java/ru/vsouth/wellbeingdiary/model/Role.java
+++ wellbeing-diary/src/main/java/ru/vsouth/wellbeingdiary/model/Role.java
@@ -1,17 +1,7 @@
 package ru.vsouth.wellbeingdiary.model;
 
 public enum Role {
-    USER("USER"),
-    ANALYST("ANALYST"),
+    USER,
+    ANALYST,
     ;
-
-    private String name;
-
-    Role(String name) {
-        this.name = name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
