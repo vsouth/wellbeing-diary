c5bf17dc2003f8a259bd78cf3c68061818f480a0
vsouth <neppchela@gmail.com>
2024-03-15 16:37:49 +0300

    switched from rest api to web controller (users)

--- wellbeing-diary/src/main/java/ru/vsouth/wellbeingdiary/controller/UserController.java
+++ wellbeing-diary/src/main/java/ru/vsouth/wellbeingdiary/controller/UserController.java
@@ -1,12 +1,22 @@
 package ru.vsouth.wellbeingdiary.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.vsouth.wellbeingdiary.dto.UserRequest;
 import ru.vsouth.wellbeingdiary.dto.UserResponse;
+import ru.vsouth.wellbeingdiary.model.Role;
 import ru.vsouth.wellbeingdiary.service.user.UserService;
 
-@RestController
-@RequestMapping("api/users")
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("user")
 public class UserController {
     private final UserService userService;
 
@@ -14,28 +24,46 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/get_users_count")
-    public Integer getUsersCount() {
-        return userService.getUsersCount();
-    }
-
     @GetMapping("/{id}")
-    public UserResponse findUserById(@PathVariable int id) {
-        return userService.getUserById(id);
+    public String showUserInfo(@PathVariable int id, Model model) {
+        UserResponse userResponse = userService.getUserById(id);
+        model.addAttribute("userResponse", userResponse);
+        List<String> roles = Arrays.stream(Role.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+        model.addAttribute("roles", roles);
+        return "user_info";
     }
 
-    @PutMapping("/update")
-    public UserResponse updateUser(@RequestBody UserRequest user) {
-        return userService.updateUser(user);
+    @PostMapping("/update")
+    public String updateUser(@ModelAttribute("userRequest") UserRequest user, Model model, RedirectAttributes redirectAttributes) {
+        UserResponse updatedUser = userService.updateUser(user);
+        model.addAttribute("userResponse", updatedUser);
+        List<String> roles = Arrays.stream(Role.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+        model.addAttribute("roles", roles);
+        redirectAttributes.addAttribute("id", updatedUser.getId());
+        return "redirect:/user/{id}";
     }
 
-    @PutMapping("/update_password")
-    public UserResponse updateUserPassword(@RequestBody UserRequest user) {
-        return userService.updateUserPassword(user);
+    @PostMapping("/update_password")
+    public ResponseEntity<String> updateUserPassword(@ModelAttribute("userRequest") UserRequest user) {
+        UserResponse userResponse = userService.updateUserPassword(user);
+        if (userResponse != null) {
+            return ResponseEntity.ok("Пароль изменен");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Пользователь не найден");
+        }
     }
 
-    @DeleteMapping("/delete/{id}")
-    public UserResponse deleteUser(@PathVariable int id) {
-        return userService.deleteUser(id);
+    @PostMapping("/delete")
+    public ResponseEntity<String> deleteUser(@ModelAttribute("userRequest") UserRequest user) {
+        UserResponse deletedUser = userService.deleteUser(user.getId());
+        if (deletedUser != null) {
+            return ResponseEntity.ok("Пользователь успешно удален");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Пользователь не найден");
+        }
     }
 }
