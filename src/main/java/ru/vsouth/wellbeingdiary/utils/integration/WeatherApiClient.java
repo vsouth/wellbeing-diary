d70fcda2e7b9c4bf62c33d2750f5f6a3312058e3
vsouth <neppchela@gmail.com>
2024-03-25 18:54:07 +0300

    added javadoc for weather api client

--- src/main/java/ru/vsouth/wellbeingdiary/utils/integration/WeatherApiClient.java
+++ src/main/java/ru/vsouth/wellbeingdiary/utils/integration/WeatherApiClient.java
@@ -6,6 +6,9 @@ import org.springframework.web.client.RestTemplate;
 import ru.vsouth.wellbeingdiary.dto.integration.WeatherApiCurrentResponse;
 import ru.vsouth.wellbeingdiary.dto.integration.WeatherApiGeoResponse;
 
+/**
+ * Клиент для работы с API погоды.
+ */
 @Component
 public class WeatherApiClient {
     final String CURRENT_WEATHER_API_URL = "https://api.open-meteo.com/v1/forecast";
@@ -16,6 +19,13 @@ public class WeatherApiClient {
         this.weatherApiMapper = weatherApiMapper;
     }
 
+    /**
+     * Метод для получения координат города по его названию
+     *
+     * @param cityName Город
+     * @return Географические координаты города
+     * @throws JsonProcessingException Исключение при обработке JSON
+     */
     public WeatherApiGeoResponse getCoordinatesByCity(String cityName) throws JsonProcessingException {
         String url = GEO_API_URL + "?name=" + cityName + "&count=1&language=ru&format=json";
         RestTemplate restTemplate = new RestTemplate();
@@ -23,6 +33,14 @@ public class WeatherApiClient {
         return weatherApiMapper.toWeatherApiGeoResponse(response);
     }
 
+    /**
+     * Метод для получения текущей погоды по координатам
+     *
+     * @param lat Широта
+     * @param lon Долгота
+     * @return Текущая погода
+     * @throws JsonProcessingException Исключение при обработке JSON
+     */
     public WeatherApiCurrentResponse getCurrentWeather(Double lat, Double lon) throws JsonProcessingException {
         String url = CURRENT_WEATHER_API_URL + "?latitude=" + lat + "&longitude=" + lon + "&current=temperature_2m,weather_code";
         RestTemplate restTemplate = new RestTemplate();
